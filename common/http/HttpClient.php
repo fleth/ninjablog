1965fd7193f9fa3604bdddca42d5070b8d01f41d
fleth <fleth@users.noreply.github.com>
2016-07-31 21:51:19 +0900

    v1.0.1に更新

--- common/http/HttpClient.php
+++ common/http/HttpClient.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace common\http;
+require_once(__DIR__."/../../vendor/autoload.php");
 
 trait HttpClient {
 
@@ -12,21 +13,42 @@ trait HttpClient {
 	 * @return string
 	 */
 	protected function httpGet($url, $params=[]){
-		$param_strings = join('&', array_map(
-			function($key, $value){return "{$key}={$value}";},
-			array_keys($params),
-			array_values($params)
-		));
-		$request_url = "{$url}";
-		$request_url .= empty($params_strings) ? "" : "?{$param_strings}";
+		$client = new \GuzzleHttp\Client();
+		$response = $client->request("GET", $url, ["query" => $params]);
 
-		/** TODO: warn発生時の検知方法が複雑になるためfile_get_contents以外の方法に変更する */
-		return file_get_contents($request_url);
+		return $response;
 
 	}
 
-	/** TODO: file_get_contents以外の方法に変更した際に実装する */
-	protected function httpPost(){}
-	protected function httpPut(){}
-	protected function httpDelete(){}
+	/**
+	 * 指定したURLにPOSTする
+	 *
+	 * @param string $url
+	 * @param array $params
+	 * @return string
+	 */
+	protected function httpPost($url, $params=[]){
+		$client = new \GuzzleHttp\Client();
+		$response = $client->request("POST", $url, ["body" => $params]);
+
+		return $response;
+	}
+
+	/**
+	 * 指定したURLにPUTする
+	 *
+	 * @param string $url
+	 * @param array $params
+	 * @return string
+	 */
+	protected function httpPut($url, $params=[]){
+		$client = new \GuzzleHttp\Client();
+		$response = $client->request("PUT", $url, ["body" => $params]);
+
+		return $response;
+	}
+
+	protected function httpDelete(){
+		throw new \Exception("DELETE method is no supported");
+	}
 }
